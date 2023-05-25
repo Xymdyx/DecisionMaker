a5aa645e9e60265745bf2c7d7ad003691dec1da1
Sam Ford <xymdyxthequirky892911@gmail.com>
2023-05-24 22:26:06 -0400

    Write doMenuLoop.  Add calls to skeletons

--- Sections/ProfileSection/ProfileSection.cs
+++ Sections/ProfileSection/ProfileSection.cs
@@ -5,12 +5,34 @@ namespace DecisionMaker
         // need profile directory path
         private const string PROFILE_DEFAULT_PATH = ".\\ProfileStorage\\profile.txt";
         private const string DEFAULT_GREETING = "Hello there, friend!";
+        private const string PROFILE_MENU_GREETING = "Welcome to the Profile Menu. This is where you can customize this program's configurable messages!";
         private const string DEFAULT_EXIT_MSG = "Goodbye, friend. We hope you found what you were looking for!";
         public ProfileSection()
-        {}
-        private void writeMenu(){}
-        public int doMenuLoop(){return 0;}
-        private int promptUser(){return 0;}
+        {
+            scanForConfigurations();
+        }
+        public int doMenuLoop()
+        {
+            Console.WriteLine(PROFILE_MENU_GREETING);
+            int opt = MenuUtils.INVALID_OPT;
+            do
+            {
+                writeMenu();
+                MenuUtils.printExitChoice();
+                opt = MenuUtils.promptUser();
+                processMenuInput(opt);
+            } while (MenuUtils.isChoiceMenuExit(opt));
+            return 0;
+        }
+        private void writeMenu()
+        {
+            /* TODO: 5/24/23
+            1. choose which component to customize
+            2. choose which part of the selected component to modify
+            3. Confirm change with a Binary confirmation
+            4. Store customizations in txts and read from them at initialization
+            */
+        }
         private void processMenuInput(int opt){}
         private void readExistingLists(){}
         private void addItemToList(){}
@@ -19,7 +41,6 @@ namespace DecisionMaker
         private void changeUsername(){}
         private void changeGreeting(){}
         private void changeExitMsg(){}
-        private void modifyRNGRange(){}
         private void scanForConfigurations(){}
     }
 }
