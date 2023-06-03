0ae0e9fa651fa676f3164ec99acaf1b1998e50f5
Sam Ford <xymdyxthequirky892911@gmail.com>
2023-06-02 20:48:06 -0400

    Add TODOs to file section

--- Sections/FileSection/FilesSection.cs
+++ Sections/FileSection/FilesSection.cs
@@ -2,15 +2,19 @@ namespace DecisionMaker
 {
     public class FilesSection:IDecisionMakerSection
     {
-        private void writeMenu(){}
-        private int doMenuLoop(){return 0;}
-        private int promptUser(){return 0;}
+        private readonly string[] fileActions = {"Delete a decision category", "Delete all decision categories",
+                                                "Clear greeting", "Clear exiting", "Clear display name", "Clear profile" };
+
+        private void writeMenu()
+        {
+            // 1. implement decision category deletion
+            // 2. implement profile clearing
+            // 3. implement decision cateogry deletion restoration
+            // 4. clear wipcat file
+        }
+
+        public int doMenuLoop(){return 0;}
         private void processMenuInput(int opt){}
-        private void printSavedCategories(){}
-        private void readExistingLists(){}
-        private void askUserForFutureItems(){}
-        private void addItemToList(){}
-        private void printCategoryChoices(){}
         private void decideForUser(List<string> choices){}
         private int runRNG(){return 0;}
     }
